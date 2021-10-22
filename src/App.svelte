9f6feb9d7677b355de1010aa1a0b63ad0e27c3e2
Tom Szwaja <th.szwaja@gmail.com>
2021-10-22 13:46:14 +0200

    chore: setup tailwind CSS

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,37 @@
 <script lang="ts">
-	export let name: string;
+    export let name: "Adam" | "Eve" | "Snake" | "God" | "Goat Person";
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <h1>Hello {name}!</h1>
+    <button
+        class="text-red-400 uppercase bg-transparent p-3 border-2 border-red-400 rounded-2xl hover:text-red-700 hover:border-red-700"
+        >Regenerate</button
+    >
 </main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+<style global lang="postcss">
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
 </style>
